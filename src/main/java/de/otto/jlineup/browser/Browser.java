81410f27d6853e2131796169d9955cb612e6b623
Marco Geweke <marco.geweke@otto.de>
2016-08-18 19:34:03 +0200

    jlineup|Test difference image generation, some changes

--- src/main/java/de/otto/jlineup/browser/Browser.java
+++ src/main/java/de/otto/jlineup/browser/Browser.java
@@ -25,8 +25,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Browser {
 
-    public static final int WINDOW_HEIGHT = 800;
-
     public enum Type {
         @SerializedName(value = "Firefox", alternate = {"firefox", "FIREFOX"})
         FIREFOX,
@@ -45,42 +43,12 @@ public class Browser {
 
 
     public void browseAndTakeScreenshots(Config config, boolean before) throws IOException, InterruptedException {
-
         WebDriver driver = getWebDriverByConfig(config);
-
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-        takeScreenshots(config, before, driver);
-        if (!before) {
-            generateDifferenceImages(config);
-        }
+        takeScreenshots(driver, config, before);
     }
 
-    private void generateDifferenceImages(Config config) throws IOException {
-
-        Map<String, UrlConfig> urls = config.getUrls();
-        for (String url : urls.keySet()) {
-            generateDifferenceImagesForUrl(config, url);
-        }
-    }
-
-
-    private void generateDifferenceImagesForUrl(Config config, String url) throws IOException {
-        UrlConfig urlConfig = config.getUrls().get(url);
-        List<Integer> resolutions = urlConfig.resolutions;
-        List<String> paths = urlConfig.paths;
-        for (String path : paths) {
-            generateDifferenceImagesForPath(config, url, resolutions, path);
-        }
-    }
-
-    private void generateDifferenceImagesForPath(Config config, String url, List<Integer> resolutions, String path) throws IOException {
-        for (Integer resolution : resolutions) {
-            generateDifferenceImage(url, path, resolution);
-        }
-    }
-
-    private void takeScreenshots(Config config, boolean before, WebDriver driver) throws IOException, InterruptedException {
+    private void takeScreenshots(WebDriver driver, Config config, boolean before) throws IOException, InterruptedException {
         try {
             Map<String, UrlConfig> urls = config.getUrls();
             for (String url : urls.keySet()) {
@@ -106,10 +74,7 @@ public class Browser {
 
     private void takeScreenshotsForPath(Config config, boolean before, WebDriver driver, String url, List<Integer> resolutions, String path) throws IOException, InterruptedException {
         for (Integer resolution : resolutions) {
-            takeSingleScreenshot(driver, config, url, path, resolution, before);
-            if (!before) {
-                generateDifferenceImage(url, path, resolution);
-            }
+            takeScreenshots(driver, config, url, path, resolution, before);
         }
     }
 
@@ -133,7 +98,7 @@ public class Browser {
         return driver;
     }
 
-    private void takeSingleScreenshot(WebDriver driver, Config config, String url, String path, int width, boolean before) throws IOException, InterruptedException {
+    private void takeScreenshots(WebDriver driver, Config config, String url, String path, int width, boolean before) throws IOException, InterruptedException {
 
         driver.manage().window().setPosition(new Point(0, 0));
         driver.manage().window().setSize(new Dimension(width, config.getWindowHeight()));
@@ -145,12 +110,15 @@ public class Browser {
         Long viewportHeight = (Long)(jse.executeScript("return document.documentElement.clientHeight"));
 
         Thread.sleep(Math.round(config.getAsyncWait() * 1000));
-        for (long yPosition = 0; yPosition < pageHeight; yPosition += viewportHeight) {
+        for (int yPosition = 0; yPosition < pageHeight; yPosition += viewportHeight) {
             File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             final BufferedImage image = ImageIO.read(screenshot);
-            ImageIO.write(image, "png", new File(getFullFileNameWithPath(url, path, width, yPosition + (before ? "_before" : "_after"))));
+            ImageIO.write(image, "png", new File(getFullFileNameWithPath(url, path, width, yPosition, before ? "before" : "after")));
+            if (!before) {
+                generateDifferenceImage(url, path, width, yPosition);
+            }
             jse.executeScript("window.scrollBy(0," + viewportHeight + ")", "");
-            Thread.sleep(500);
+            Thread.sleep(50);
         }
     }
 
@@ -164,11 +132,11 @@ public class Browser {
         return url + path;
     }
 
-    void generateDifferenceImage(String url, String path, int width) throws IOException {
+    void generateDifferenceImage(String url, String path, int width, int yPosition) throws IOException {
 
         BufferedImage imageBefore;
         final String before = "before";
-        final String fullFileNameWithPath = getFullFileNameWithPath(url, path, width, before);
+        final String fullFileNameWithPath = getFullFileNameWithPath(url, path, width, yPosition, before);
         try {
             imageBefore = ImageIO.read(new File(fullFileNameWithPath));
         } catch (IIOException e) {
@@ -176,15 +144,15 @@ public class Browser {
             throw e;
         }
         final String after = "after";
-        BufferedImage imageAfter = ImageIO.read(new File(getFullFileNameWithPath(url, path, width, after)));
-        ImageIO.write(ImageUtils.getDifferenceImage(imageBefore, imageAfter), "png", new File(getFullFileNameWithPath(url, path, width, "DIFFERENCE")));
+        BufferedImage imageAfter = ImageIO.read(new File(getFullFileNameWithPath(url, path, width, yPosition, after)));
+        ImageIO.write(ImageUtils.getDifferenceImage(imageBefore, imageAfter), "png", new File(getFullFileNameWithPath(url, path, width, yPosition, "DIFFERENCE")));
     }
 
-    String getFullFileNameWithPath(String url, String path, int width, String step) {
-        return parameters.getWorkingDirectory() + (parameters.getWorkingDirectory().endsWith("/") ? "" : "/") + generateFileName(url, path, width, step);
+    String getFullFileNameWithPath(String url, String path, int width, int yPosition, String step) {
+        return parameters.getWorkingDirectory() + (parameters.getWorkingDirectory().endsWith("/") ? "" : "/") + generateFileName(url, path, width, yPosition, step);
     }
 
-    static String generateFileName(String url, String path, int width, String type) {
+    static String generateFileName(String url, String path, int width, int yPosition, String type) {
 
         if (path.equals("/") || path.equals("")) {
             path = "root";
@@ -192,7 +160,7 @@ public class Browser {
         if (url.endsWith("/")) {
             url = url.substring(0, url.length() - 1);
         }
-        String fileName = url + "_" + path + "_" + width + "_" + type;
+        String fileName = url + "_" + path + "_" + width + "_" + yPosition + "_" + type;
         fileName = fileName.replace("http://", "");
         fileName = fileName.replace("https://", "");
         fileName = fileName.replace("/", "_");
