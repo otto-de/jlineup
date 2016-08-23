d9e4048b2b10ff1bbb4f44aafd8c1df99b29840e
Marco Geweke <marco.geweke@otto.de>
2016-08-23 18:35:49 +0200

    jlineup|Two new config items

--- src/test/java/de/otto/jlineup/browser/TestSupportWebDriver.java
+++ src/test/java/de/otto/jlineup/browser/TestSupportWebDriver.java
@@ -5,6 +5,5 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public interface TestSupportWebDriver extends WebDriver, JavascriptExecutor, TakesScreenshot {
-
-
+    //Just an interface for mocking
 }
