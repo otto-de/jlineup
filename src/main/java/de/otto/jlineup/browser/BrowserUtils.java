ee1a8bbfd9df4fb640c01c0521ba12c921b2dc4d
Marco Geweke <marco.geweke@otto.de>
2017-02-23 18:12:01 +0100

    Put old chrome option back

--- src/main/java/de/otto/jlineup/browser/BrowserUtils.java
+++ src/main/java/de/otto/jlineup/browser/BrowserUtils.java
@@ -56,7 +56,7 @@ public class BrowserUtils {
                 ChromeDriverManager.getInstance().setup();
                 ChromeOptions options = new ChromeOptions();
                 //To work in a headless env, this is needed
-                options.addArguments("--disable-setuid-sandbox");
+                options.addArguments("--no-sandbox");
                 driver = new ChromeDriver(options);
                 break;
             case PHANTOMJS:
