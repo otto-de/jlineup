fd5a9146a3ee6640b6a5cd9d737b8f6a7a987907
Marco Geweke <marco.geweke@otto.de>
2017-07-25 11:24:46 +0200

    Test travis without acceptance tests

--- src/test/java/de/otto/jlineup/acceptance/JLineupAcceptanceTest.java
+++ src/test/java/de/otto/jlineup/acceptance/JLineupAcceptanceTest.java
@@ -21,7 +21,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
+@Ignore
 public class JLineupAcceptanceTest {
 
     @Rule
