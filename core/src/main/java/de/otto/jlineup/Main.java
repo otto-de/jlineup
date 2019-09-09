b936127aa324ffef8fed8698e7c2eb1055a5a81b
Marco Geweke <marco.geweke@gmail.com>
2019-09-09 09:23:42 +0200

    Fix oss sonatype release

---
+++ core/src/main/java/de/otto/jlineup/Main.java
@@ -0,0 +1,9 @@
+package de.otto.jlineup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The core module has no function, use jlineup-cli or jlineup-web!");
+    }
+
+}
