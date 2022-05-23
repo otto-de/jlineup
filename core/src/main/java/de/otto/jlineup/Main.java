5052f74c4637a04817037febfb0e07595fabd288
MediaMarco <marco.geweke@otto.de>
2022-05-23 08:57:12 +0200

    Change hint text of core's main
    Co-authored-by: Marco Geweke <marco.geweke@otto.de>

--- core/src/main/java/de/otto/jlineup/Main.java
+++ core/src/main/java/de/otto/jlineup/Main.java
@@ -3,7 +3,7 @@ package de.otto.jlineup;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("The core module has no function, use jlineup-cli or jlineup-web!");
+        System.out.println("The core module has no standalone functionality. Use jlineup-cli or jlineup-web!");
     }
 
 }
