8d30cc6da47c57671536d8f2cc6f76b698d36f1c
Marco Geweke <marco.geweke@gmail.com>
2022-11-04 22:34:16 +0100

    Replace all dots and forward and backward slashes from fileNamePrefix

--- core/src/main/java/de/otto/jlineup/file/FileService.java
+++ core/src/main/java/de/otto/jlineup/file/FileService.java
@@ -149,7 +149,7 @@ public class FileService {
         String fileNamePrefix = url + DIVIDER + urlSubPath + DIVIDER;
         fileNamePrefix = fileNamePrefix.replace("://", DIVIDER);
         fileNamePrefix = fileNamePrefix.replaceAll("[^A-Za-z0-9\\-_]", DIVIDER);
-        fileNamePrefix = fileNamePrefix.replaceAll("\\.", "").replaceAll("/", "");
+        fileNamePrefix = fileNamePrefix.replace("\\", "").replace("/", "").replace(".", "");
 
         if (fileNamePrefix.length() > MAX_URL_TO_FILENAME_LENGTH) {
             fileNamePrefix = fileNamePrefix.substring(0, MAX_URL_TO_FILENAME_LENGTH) + DIVIDER;
