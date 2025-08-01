dd0b68563913e4dc565f7daa090ba43252b735f9
MediaMarco <marco.geweke@gmail.com>
2025-08-01 10:03:11 +0200

    Remove unneccessary usage of assertj
    Co-authored-by: Marco Geweke <marco.geweke@otto.de>

--- core/src/test/java/de/otto/jlineup/JLineupRunnerTest.java
+++ core/src/test/java/de/otto/jlineup/JLineupRunnerTest.java
@@ -4,10 +4,10 @@ import de.otto.jlineup.config.JobConfig;
 import de.otto.jlineup.config.UrlConfig;
 import de.otto.jlineup.report.Summary;
 import de.otto.jlineup.report.UrlReport;
-import org.assertj.core.data.MapEntry;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,7 +18,7 @@ public class JLineupRunnerTest {
     public void shouldNotFailIfMaxDiffIsSameAsDetectedDiff() {
 
         UrlReport urlReport = new UrlReport(null, new Summary(false, 0, 15.1234567890123456, 0));
-        boolean detectedDifferenceGreaterThanMaxDifference = JLineupRunner.isDetectedDifferenceGreaterThanMaxDifference(Collections.singleton(MapEntry.entry("abc", urlReport)), JobConfig.exampleConfigBuilder().withUrls(Collections.singletonMap("abc", UrlConfig.urlConfigBuilder().withMaxDiff(15.1234567890123456).build())).build());
+        boolean detectedDifferenceGreaterThanMaxDifference = JLineupRunner.isDetectedDifferenceGreaterThanMaxDifference(Collections.singleton(Map.entry("abc", urlReport)), JobConfig.exampleConfigBuilder().withUrls(Collections.singletonMap("abc", UrlConfig.urlConfigBuilder().withMaxDiff(15.1234567890123456).build())).build());
 
         assertThat(detectedDifferenceGreaterThanMaxDifference, is(false));
     }
