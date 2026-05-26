eb5fd0ea566d8ac7fd620231904f413ba2d4ebb1
MediaMarco <marco.geweke@gmail.com>
2026-05-26 09:33:58 +0200

    Log server url at application startup (for convenience)
    Co-authored-by: Marco Geweke <marco.geweke@otto.de>

--- web/src/main/java/de/otto/jlineup/web/JLineupWebApplication.java
+++ web/src/main/java/de/otto/jlineup/web/JLineupWebApplication.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.server.context.WebServerInitializedEvent;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 import static java.lang.invoke.MethodHandles.lookup;
 
@@ -22,5 +25,24 @@ public class JLineupWebApplication {
 		LOG.info("\nStarting JLineup {}\n", Utils.getVersion());
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(JLineupWebApplication.class);
 		builder.headless(false).run(args);
+	}
+
+	@Component
+	static class StartupLogger {
+		@EventListener
+		public void onWebServerReady(WebServerInitializedEvent event) {
+			int port = event.getWebServer().getPort();
+			String contextPath = event.getApplicationContext().getEnvironment()
+					.getProperty("server.servlet.context-path", "");
+			if (contextPath.equals("/")) {
+				contextPath = "";
+			}
+			String address = event.getApplicationContext().getEnvironment()
+					.getProperty("server.address", "localhost");
+			if (address.equals("0.0.0.0") || address.equals("::")) {
+				address = "localhost";
+			}
+			LOG.info("\n\n --- http://{}:{}{}/internal/status ---\n", address, port, contextPath);
+		}
 	}
 }
